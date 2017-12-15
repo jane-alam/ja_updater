560c37f86469566306cc4490d0c1067f9b176d88
Mahammad Jan-E-Alam <janealam416@gmail.com>
2017-12-16 02:58:30 +0600

    Create ja_updater.php
    Main file of plugin

---
+++ ja_updater.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Plugin Name:   JaneAlam Updater
+ * Plugin URI:    http://janealam.com/wordpress-plugins/ja-updater
+ * Description:   The ultimate WordPress Self-hoster Theme Updater
+ * Author:        Jane Alam
+ * Author URI:    http://janealam.com
+ * Version:       0.0.1
+ * Text Domain:   jane_alam
+ *
+ * GitHub Plugin URI: jane-alam/ja_updater
+ * GitHub Plugin URI: https://github.com/jane-alam/ja_updater
+ *
+ * @package     ja_updater
+ * @category    Core
+ * @author      Jane Alam
+ * @copyright   Copyright (c) 2017, Jane Alam
+ * @license     http://opensource.org/licenses/https://opensource.org/licenses/MIT
+ * @since       1.0
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+$MyThemeUpdateChecker = new ThemeUpdateChecker(
+	'theme-slup', //Theme slug. Usually the same as the name of its directory.
+	'http://i.janealam.com/info.json' //Metadata URL.
+);
